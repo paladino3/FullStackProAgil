bfead118d85c3c2c6b77d41c4062a20dbcafabd7
wesley rolim simoes <rolim.r@icloud.com>
2020-01-10 14:36:03 -0300

    Adicionado o projeto Angular ProAgil-App SPA

--- ProAgil.Api/Startup.cs
+++ ProAgil.Api/Startup.cs
@@ -30,6 +30,7 @@ namespace ProAgil.Api
             services.AddDbContext<DataContext>(x=> x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,7 @@ namespace ProAgil.Api
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
