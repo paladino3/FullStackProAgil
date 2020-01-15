515c252efc3d5992defa832f01ce7e89131609e7
Wesley rolim <rolim.r@icloud.com>
2020-01-15 15:59:39 -0300

    exclusao de um evento

--- ProAgil-App/src/app/_services/Evento.service.ts
+++ ProAgil-App/src/app/_services/Evento.service.ts
@@ -24,5 +24,11 @@ export class EventoService {
   postEvento(evento: Evento) {
     return this.http.post(this.baseURL, evento);
   }
+  putEvento(evento: Evento) {
+    return this.http.put(`${this.baseURL}/${evento.id}`, evento);
+  }
+  deleteEvento(id: number) {
+    return this.http.delete(`${this.baseURL}/${id}`);
+  }
 
 }
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -5,6 +5,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { defineLocale, BsLocaleService, ptBrLocale } from 'ngx-bootstrap';
 import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { templateJitUrl } from '@angular/compiler';
 defineLocale('', ptBrLocale);
 
 @Component({
@@ -21,85 +22,126 @@ export class EventosComponent implements OnInit {
   imagemMargem = 2;
   mostrarImagem = false;
   registerForm: FormGroup;
+  modoSalvar = 'post';
+  bodyDeletarEvento = '';
 
-  // tslint:disable-next-line:variable-name
-  _filtroLista ='';
+// tslint:disable-next-line:variable-name
+_filtroLista = '';
 
-  constructor(
-    private eventoService: EventoService,
-    private modalService: BsModalService,
-    private fb: FormBuilder,
-    private localeService: BsLocaleService
-    ) {
-      this.localeService.use('pt-br');
+constructor(
+  private eventoService: EventoService,
+  private modalService: BsModalService,
+  private fb: FormBuilder,
+  private localeService: BsLocaleService
+  ) {
+    this.localeService.use('pt-br');
+  }
+
+get filtroLista(): string {
+  return this._filtroLista;
+}
+set filtroLista(value: string) {
+  this._filtroLista = value;
+  this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+}
+
+novoEvento(template: any)  {
+  this.modoSalvar = 'post';
+  this.openModal(template);
+}
+editarEvento(evento: Evento, template: any) {
+  this.modoSalvar = 'put';
+  this.openModal(template);
+  this.evento = evento;
+  this.registerForm.patchValue(evento);
+}
+excluirEvento(evento: Evento, template: any)  {
+  this.openModal(template);
+  this.evento = evento;
+  this.bodyDeletarEvento = `Você tem certeza que deseja excluir o evento? Código ${evento.id},  Sobre: ${evento.tema}`;
+}
+
+confirmeDelete(template: any) {
+  this.eventoService.deleteEvento(this.evento.id).subscribe(
+    () => {
+      template.hide();
+      this.getEventos();
+    }, error => {
+      console.log(error);
     }
-
-  get filtroLista(): string {
-    return this._filtroLista;
-  }
-  set filtroLista(value: string) {
-    this._filtroLista = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-  openModal(template: any) {
-    this.registerForm.reset();
-    template.show();
+    );
   }
 
-  ngOnInit() {
-    this.validation();
-    this.getEventos();
-  }
+openModal(template: any) {
+  this.registerForm.reset();
+  template.show();
+}
 
-  filtrarEventos(filtrarPor: string): Evento[] {
-    filtrarPor =  filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
-  }
+ngOnInit() {
+  this.validation();
+  this.getEventos();
+}
 
+filtrarEventos(filtrarPor: string): Evento[] {
+  filtrarPor =  filtrarPor.toLocaleLowerCase();
+  return this.eventos.filter(
+    evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
+  }
 
   alternarImg() {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
-  validation() {
-    this.registerForm = this.fb.group({
-      tema:  ['',
-       [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
-      local:  ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
-      dataEvento:  ['', Validators.required],
-      imagemUrl:  ['', Validators.required],
-      qtdePessoas:  ['',
-      [Validators.required, Validators.max(5000)]],
-      telefone:  ['', Validators.required],
-      email:  ['',
-       [Validators.required, Validators.email]]
-       });
-  }
-  salvarAlteracao(template: any) {
-      if (this.registerForm.valid) {
-        this.evento = Object.assign({}, this.registerForm.value);
-        this.eventoService.postEvento(this.evento).subscribe(
-          (novoEvento: Evento) => {
-            console.log(novoEvento);
-            template.hide();
-            this.getEventos();
-          }, error => {
-            console.log(error);
-          }
-        );
-      }
-  }
-
-  getEventos() {
-    this.eventoService.getAllEvento().subscribe(
-      // tslint:disable-next-line:variable-name
-      (_eventos: Evento[]) => {
-        this.eventos = _eventos;
-        this.eventosFiltrados = this.eventos;
-        console.log(_eventos);
+validation() {
+  this.registerForm = this.fb.group({
+    tema:  ['',
+    [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+    local:  ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+    dataEvento:  ['', Validators.required],
+    imagemUrl:  ['', Validators.required],
+    qtdePessoas:  ['',
+    [Validators.required, Validators.max(5000)]],
+    telefone:  ['', Validators.required],
+    email:  ['',
+    [Validators.required, Validators.email]]
+  });
+}
+salvarAlteracao(template: any) {
+  if (this.registerForm.valid) {
+  if (this.modoSalvar === 'post') {
+    this.evento = Object.assign({}, this.registerForm.value);
+    this.eventoService.postEvento(this.evento).subscribe(
+      (novoEvento: Evento) => {
+        console.log(novoEvento);
+        template.hide();
+        this.getEventos();
       }, error => {
         console.log(error);
-      });
+    }
+  );
+  } else {
+    this.evento = Object.assign({id: this.evento.id}, this.registerForm.value);
+    this.eventoService.putEvento(this.evento).subscribe(
+      (novoEvento: Evento) => {
+        template.hide();
+        this.getEventos();
+      }, error => {
+        console.log(error);
+      }
+      );
     }
   }
+}
+
+getEventos() {
+  this.eventoService.getAllEvento().subscribe(
+    // tslint:disable-next-line:variable-name
+    (_eventos: Evento[]) => {
+      this.eventos = _eventos;
+      this.eventosFiltrados = this.eventos;
+      console.log(_eventos);
+    }, error => {
+      console.log(error);
+    });
+  }
+}
